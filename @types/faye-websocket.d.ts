a99c8852a389130e339d865f942bab8cbf68dd37
Leonardo Venturini <leovenbag@gmail.com>
2023-07-25 14:17:46 -0400

    add types for faye

---
+++ @types/faye-websocket.d.ts
@@ -0,0 +1,26 @@
+declare module 'faye-websocket' {
+  export interface MessageEvent {
+    data: any
+  }
+
+  export interface CloseEvent {
+    code: number
+    reason: string
+    wasClean: boolean
+  }
+
+  export class Client {
+    constructor(url: string, protocols?: Array<string> | null, options?: { [name: string]: unknown })
+
+    send(data: string): void
+
+    close(code?: number, reason?: string): void
+
+    on(event: 'open', cb: () => void): void
+    on(event: 'message', cb: (msg: MessageEvent) => void): void
+    on(event: 'close', cb: (event: CloseEvent) => void): void
+    on(event: 'error', cb: (error: Error) => void): void
+  }
+
+  export default WebSocket
+}
