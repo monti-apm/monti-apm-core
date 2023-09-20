5a6cbbeb1d326680e76757f8fb290619d121bc40
Leonardo Venturini <leovenbag@gmail.com>
2023-09-20 09:52:15 -0400

    ignore await detector tests on < node 12

--- src/await-detector.test.js
+++ src/await-detector.test.js
@@ -1,107 +1,109 @@
 import { afterEach, beforeEach, describe, it } from 'mocha';
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import { AwaitDetector, AwaitDetectorSymbol } from './await-detector';
 import { sleep } from './utils';
 
-(process.version.match(/v4/) ? describe.skip : describe)(
-  'AwaitDetector',
-  () => {
-    let detector;
+const major = parseInt(process.version.match(/v(\d+)/)[1], 10);
 
-    beforeEach(() => {
-      detector = new AwaitDetector({
-        logging: true,
-      });
+(major < 12 ? describe.skip : describe)('AwaitDetector', async () => {
+  const { AwaitDetector, AwaitDetectorSymbol } = await import(
+    './await-detector'
+  );
+
+  let detector;
+
+  beforeEach(() => {
+    detector = new AwaitDetector({
+      logging: true,
+    });
+  });
+
+  afterEach(() => {
+    detector.unregister();
+  });
+
+  describe('enable and disable', () => {
+    it('should enable and disable the hook', () => {
+      const enableSpy = spy(detector.hook, 'enable');
+      const disableSpy = spy(detector.hook, 'disable');
+
+      detector.enable();
+      expect(enableSpy.calledOnce).to.be.true;
+
+      detector.disable();
+      expect(disableSpy.calledOnce).to.be.true;
+
+      enableSpy.restore();
+      disableSpy.restore();
+    });
+  });
+
+  describe('promise constructor', () => {
+    it('should replace the global promise constructor', () => {
+      const originalPromise = AwaitDetector.OLD_PROMISE_CONSTRUCTOR;
+
+      expect(global.Promise).to.not.equal(originalPromise);
+      expect(global.Promise[AwaitDetectorSymbol]).to.be.true;
     });
 
-    afterEach(() => {
+    it('should unwrap the promise constructor', () => {
+      const originalPromise = AwaitDetector.OLD_PROMISE_CONSTRUCTOR;
+
       detector.unregister();
+
+      expect(global.Promise).to.equal(originalPromise);
+      expect(global.Promise[AwaitDetectorSymbol]).to.be.undefined;
+    });
+  });
+
+  describe('detecting await', () => {
+    it('should run onAwaitStart and onAwaitEnd', async () => {
+      console.log(detector);
+
+      const onAwaitStartSpy = spy(detector, 'onAwaitStart');
+      const onAwaitEndSpy = spy(detector, 'onAwaitEnd');
+
+      const result = await detector.detect(async () => {
+        await sleep(10);
+
+        return true;
+      });
+
+      expect(result).to.be.true;
+
+      expect(onAwaitStartSpy.calledOnce).to.be.true;
+      expect(onAwaitEndSpy.calledOnce).to.be.true;
+
+      expect(onAwaitStartSpy.getCall(0).args).to.have.length(2);
+      expect(onAwaitEndSpy.getCall(0).args).to.have.length(2);
+
+      onAwaitStartSpy.restore();
+      onAwaitEndSpy.restore();
     });
 
-    describe('enable and disable', () => {
-      it('should enable and disable the hook', () => {
-        const enableSpy = spy(detector.hook, 'enable');
-        const disableSpy = spy(detector.hook, 'disable');
+    it('should resolve awaits in the correct order', async () => {
+      const onAwaitStartSpy = spy(detector, 'onAwaitStart');
+      const onAwaitEndSpy = spy(detector, 'onAwaitEnd');
 
-        detector.enable();
-        expect(enableSpy.calledOnce).to.be.true;
-
-        detector.disable();
-        expect(disableSpy.calledOnce).to.be.true;
-
-        enableSpy.restore();
-        disableSpy.restore();
+      const result = await detector.detect(async () => {
+        await sleep(10);
+        await sleep(20);
+        await sleep(30);
+        return true;
       });
+
+      expect(result).to.be.true;
+
+      expect(onAwaitStartSpy.callCount).to.be.equal(3);
+      expect(onAwaitEndSpy.callCount).to.be.equal(3);
+
+      const starts = onAwaitStartSpy.getCalls().map((call) => call.args[0]);
+      const ends = onAwaitEndSpy.getCalls().map((call) => call.args[0]);
+
+      expect(starts).to.be.deep.equal(ends);
+
+      onAwaitStartSpy.restore();
+      onAwaitEndSpy.restore();
     });
-
-    describe('promise constructor', () => {
-      it('should replace the global promise constructor', () => {
-        const originalPromise = AwaitDetector.OLD_PROMISE_CONSTRUCTOR;
-
-        expect(global.Promise).to.not.equal(originalPromise);
-        expect(global.Promise[AwaitDetectorSymbol]).to.be.true;
-      });
-
-      it('should unwrap the promise constructor', () => {
-        const originalPromise = AwaitDetector.OLD_PROMISE_CONSTRUCTOR;
-
-        detector.unregister();
-
-        expect(global.Promise).to.equal(originalPromise);
-        expect(global.Promise[AwaitDetectorSymbol]).to.be.undefined;
-      });
-    });
-
-    describe('detecting await', () => {
-      it('should run onAwaitStart and onAwaitEnd', async () => {
-        console.log(detector);
-
-        const onAwaitStartSpy = spy(detector, 'onAwaitStart');
-        const onAwaitEndSpy = spy(detector, 'onAwaitEnd');
-
-        const result = await detector.detect(async () => {
-          await sleep(10);
-
-          return true;
-        });
-
-        expect(result).to.be.true;
-
-        expect(onAwaitStartSpy.calledOnce).to.be.true;
-        expect(onAwaitEndSpy.calledOnce).to.be.true;
-
-        expect(onAwaitStartSpy.getCall(0).args).to.have.length(2);
-        expect(onAwaitEndSpy.getCall(0).args).to.have.length(2);
-
-        onAwaitStartSpy.restore();
-        onAwaitEndSpy.restore();
-      });
-
-      it('should resolve awaits in the correct order', async () => {
-        const onAwaitStartSpy = spy(detector, 'onAwaitStart');
-        const onAwaitEndSpy = spy(detector, 'onAwaitEnd');
-
-        const result = await detector.detect(async () => {
-          await sleep(10);
-          await sleep(20);
-          await sleep(30);
-          return true;
-        });
-
-        expect(result).to.be.true;
-
-        expect(onAwaitStartSpy.callCount).to.be.equal(3);
-        expect(onAwaitEndSpy.callCount).to.be.equal(3);
-
-        const starts = onAwaitStartSpy.getCalls().map((call) => call.args[0]);
-        const ends = onAwaitEndSpy.getCalls().map((call) => call.args[0]);
-
-        expect(starts).to.be.deep.equal(ends);
-
-        onAwaitStartSpy.restore();
-        onAwaitEndSpy.restore();
-      });
-    });
-  },
-);
+  });
+});
