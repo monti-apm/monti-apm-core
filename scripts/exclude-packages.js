d590a387af4d19ff9cc23215e68dca824a024e91
Leonardo Venturini <leovenbag@gmail.com>
2023-09-22 10:02:04 -0400

    add script to exclude packages

---
+++ scripts/exclude-packages.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const packages = process.argv.slice(2);
+
+const packageJSON = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+for (const packageName of packages) {
+  delete packageJSON.dependencies[packageName];
+  delete packageJSON.devDependencies[packageName];
+}
+
+fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, 2), 'utf-8');
+
+execSync('npm install', { stdio: 'inherit' });
